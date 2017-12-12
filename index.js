992c9508edb1d4213a3a2cf39ea44520b6d44a99
Sabrina Jones <sabrina.jones87@gmail.com>
2017-12-12 22:18:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const logDriverNames = function(driversArray){
     console.log(driver.name);
   })
 }
+
+const logDriversByHometown = function(driversArray, location){
+  driversArray.filter(function(driver){
+    if (driver.hometown === location){
+      console.log(driver.name);
+    }
+  })
+}
