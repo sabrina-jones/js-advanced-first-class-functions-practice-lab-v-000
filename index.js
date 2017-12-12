d8376aefd8377a12bc72874fc82fb67b9e44a893
Sabrina Jones <sabrina.jones87@gmail.com>
2017-12-12 22:20:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,31 @@ const logDriversByHometown = function(driversArray, location){
     }
   })
 }
+
+const driversByRevenue = function(driversArray){
+  return driversArray.slice().sort(function(a,b){
+    return a.revenue - b.revenue;
+  })
+}
+
+const driversByName = function(driversArray){
+  return driversArray.slice().sort(function(a,b){
+    return a.name.localeCompare(b.name);
+  })
+}
+
+const totalRevenue = function(driversArray){
+  let revenue = 0;
+  driversArray.forEach(function(driver){
+    revenue += driver.revenue;
+  })
+  return revenue;
+}
+
+const averageRevenue = function(driversArray){
+  let average = 0;
+  driversArray.forEach(function(driver){
+    average += driver.revenue;
+  })
+  return average / driversArray.length;
+}
